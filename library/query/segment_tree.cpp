985c31ea40d7aa6fbe14b5160fc07f2c9111b5d2
dush1729 <dush1729@gmail.com>
2020-09-27 13:30:49 +0530

    segtree use auto for struct

--- library/query/segment_tree.cpp
+++ library/query/segment_tree.cpp
@@ -12,7 +12,7 @@ struct node
 
 void build(int node,int start,int end)
 {
-	struct node &parent=tree[node];
+	auto &parent=tree[node];
 	if(start==end) parent.val=a[start];
 	else
 	{
@@ -20,15 +20,15 @@ void build(int node,int start,int end)
 		build(node<<1,start,mid);
 		build((node<<1)+1,mid+1,end);
 
-		struct node left_child=tree[node<<1];
-		struct node right_child=tree[(node<<1)+1];
+		auto left_child=tree[node<<1];
+		auto right_child=tree[(node<<1)+1];
 		parent.val=left_child.val+right_child.val;
 	}
 }
 
 void point_update(int node,int start,int end,int idx,int val)
 {
-	struct node &parent=tree[node];
+	auto &parent=tree[node];
 	if(start==end)
 	{
 		a[idx]+=val;
@@ -40,8 +40,8 @@ void point_update(int node,int start,int end,int idx,int val)
 		if(start<=idx and idx<=mid) point_update(node<<1,start,mid,idx,val);
 		else point_update((node<<1)+1,mid+1,end,idx,val);
 
-		struct node left_child=tree[node<<1];
-		struct node right_child=tree[(node<<1)+1];
+		auto left_child=tree[node<<1];
+		auto right_child=tree[(node<<1)+1];
 		parent.val=left_child.val+right_child.val;
 	}
 }
@@ -49,7 +49,7 @@ void point_update(int node,int start,int end,int idx,int val)
 int no_lazy_query(int node,int start,int end,int l,int r)
 {
 	if(r<start or end<l) return 0;
-	struct node &parent=tree[node];
+	auto &parent=tree[node];
 	if(l<=start and end<=r) return parent.val;
 
 	int mid=(start+end)>>1;
@@ -61,7 +61,7 @@ int no_lazy_query(int node,int start,int end,int l,int r)
 void no_lazy_update(int node,int start,int end,int l,int r,int val)
 {
 	if(start>end or start>r or end<l) return;
-	struct node &parent=tree[node];
+	auto &parent=tree[node];
 	if(start==end)
 	{
 		parent.val+=val;
@@ -71,14 +71,14 @@ void no_lazy_update(int node,int start,int end,int l,int r,int val)
 	int mid=(start+end)>>1;
 	no_lazy_update(node<<1,start,mid,l,r,val);
 	no_lazy_update((node<<1)+1,mid+1,end,l,r,val);
-	struct node left_child=tree[node<<1];
-	struct node right_child=tree[(node<<1)+1];
+	auto left_child=tree[node<<1];
+	auto right_child=tree[(node<<1)+1];
 	parent.val=left_child.val+right_child.val;
 }
 
 void update(int node,int start,int end,int l,int r,int val)
 {
-	struct node &parent=tree[node];
+	auto &parent=tree[node];
 	if(lazy[node]!=0)
 	{
 		parent.val+=(end-start+1)*lazy[node];
@@ -106,15 +106,15 @@ void update(int node,int start,int end,int l,int r,int val)
 	int mid=(start+end)>>1;
 	update(node<<1,start,mid,l,r,val);
 	update((node<<1)+1,mid+1,end,l,r,val);
-	struct node left_child=tree[node<<1];
-	struct node right_child=tree[(node<<1)+1];
+	auto left_child=tree[node<<1];
+	auto right_child=tree[(node<<1)+1];
 	parent.val=left_child.val+right_child.val;
 }
 
 int query(int node,int start,int end,int l,int r)
 {
 	if(start>end or start>r or end<l) return 0;
-	struct node &parent=tree[node];
+	auto &parent=tree[node];
 	if(lazy[node]!=0)
 	{
 		parent.val+=(end-start+1)*lazy[node];
@@ -131,7 +131,7 @@ int query(int node,int start,int end,int l,int r)
 	int mid=(start+end)>>1;
 	int p1=query(node<<1,start,mid,l,r);
 	int p2=query((node<<1)+1,mid+1,end,l,r);
-	return (p1 + p2);
+	return (p1+p2);
 }
 
 int main()
