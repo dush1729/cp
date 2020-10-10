211803871e5694f94f98388b31befe72ae6d06e4
dush1729 <dush1729@gmail.com>
2020-10-10 22:23:58 +0530

    add binarysearch.com template

---
+++ library/templates/bs.cpp
@@ -0,0 +1,17 @@
+#include "solution.hpp"
+using namespace std;
+
+class Solution {
+	public:
+	int solve(vector<int>& a) {
+		return 0;
+	}
+};
+
+#ifdef LOCAL
+int main()
+{
+	vector <int> v={1,2};
+	auto ans=Solution().solve(v);
+}
+#endif
