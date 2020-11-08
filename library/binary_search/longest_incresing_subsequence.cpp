ff6f3f9ca86fbacfbdd165f9a8614d07c60bca6e
dush1729 <dush1729@gmail.com>
2020-11-08 23:32:32 +0530

    remove lis redundant code

--- library/binary_search/longest_incresing_subsequence.cpp
+++ library/binary_search/longest_incresing_subsequence.cpp
@@ -12,12 +12,12 @@ int main()
 	cin>>n;
 	for(int i=1;i<=n;i++) cin>>a[i];
 
-	v.push_back(a[1]);
-	for(int i=2;i<=n;i++)
+	for(int i=1;i<=n;i++)
 	{
-		auto it=lower_bound(v.begin(),v.end(),a[i])-v.begin();
-		if(it==v.size()) v.push_back(a[i]);
-		else v[it]=a[i];
+		int j=lower_bound(v.begin(),v.end(),a[i])-v.begin();
+		if(j==v.size()) v.push_back(a[i]);
+		else v[j]=a[i];
 	}
+
 	cout<<v.size()<<endl;
 }
