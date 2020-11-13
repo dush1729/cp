a6f64a75569a279bc936240de87dd9016d1629bf
dush1729 <dush1729@gmail.com>
2020-11-14 01:44:32 +0530

    minor optimizations for coordinate compressions

--- library/misc/coordinate_compression.cpp
+++ library/misc/coordinate_compression.cpp
@@ -5,12 +5,15 @@ https://binarysearch.com/problems/Longest-Repeating-Sublist-After-K-Updates
 
 void compress(vector <int> &v)
 {
+	if(v.empty()) return;
 	vector <int> s=v;
 	sort(s.begin(),s.end());
 
 	int j=1;
-	map <int,int> m;
-	for(auto i:s) m[i]=j++;
+	unordered_map <int,int> m;
+	m[s[0]]=j++;
+	for(int i=1;i<s.size();i++)
+		if(s[i]!=s[i-1]) m[s[i]]=j++;
 
 	for(auto &i:v) i=m[i];
 }
