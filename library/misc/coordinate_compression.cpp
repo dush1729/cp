c0cb49234b11851813a0481bd61b1c8f7f10d800
dush1729 <dush1729@gmail.com>
2020-12-04 21:58:27 +0530

    coordinate compression: return max value

--- library/misc/coordinate_compression.cpp
+++ library/misc/coordinate_compression.cpp
@@ -3,17 +3,17 @@ https://codeforces.com/contest/61/problem/E
 https://binarysearch.com/problems/Longest-Repeating-Sublist-After-K-Updates
 */
 
-void compress(vector <int> &v)
-{
-	if(v.empty()) return;
-	vector <int> s=v;
-	sort(s.begin(),s.end());
+int compress(vector <int> &v) {
+	if(v.empty()) return 0;
+	vector <int> s = v;
+	sort(s.begin(), s.end());
 
-	int j=1;
-	unordered_map <int,int> m;
-	m[s[0]]=j++;
-	for(int i=1;i<s.size();i++)
-		if(s[i]!=s[i-1]) m[s[i]]=j++;
+	int j = 1;
+	unordered_map <int, int> m;
+	m[s[0]] = j++;
+	for(int i = 1; i < s.size(); i++)
+		if(s[i] != s[i - 1]) m[s[i]] = j++;
 
 	for(auto &i:v) i=m[i];
+	return j;
 }
