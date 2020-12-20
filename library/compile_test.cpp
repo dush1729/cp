94db5cf48aecfad7c3d96c6c9d3c1d699297a3b9
dush1729 <dush1729@gmail.com>
2020-12-20 12:45:28 +0530

    refactor hashing

---
+++ library/compile_test.cpp
@@ -0,0 +1,7 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#include "string/hashing.cpp"
+
+int main() {
+}
--- library/string/hashing.cpp
+++ library/string/hashing.cpp
@@ -1,114 +1,100 @@
-#include "bits/stdc++.h"
-using namespace std;
+/*
 
-//CNT - 26 for alphabet, 10 for decimal
-//START - 'a' for alphabet, '0' for decimal
-const int CNT=10;
-const char START='0';
-const vector <int> MOD={1000000007,573161801};
+https://www.interviewbit.com/problems/longest-palindromic-substring/
 
-vector <long long> calculate_left_hash(string a,int mod)
-{
-	vector <long long> v;
-	v.push_back(0);
-	for(int i=0;i<a.size();i++) v.push_back((v.back()*CNT+a[i]-START)%mod);
-	return v;
-}
+*/
 
-vector <long long> calculate_right_hash(string a,int mod)
-{
-	reverse(a.begin(),a.end());
-	vector <long long> v;
-	v.push_back(0);
-	for(int i=0;i<a.size();i++) v.push_back((v.back()*CNT+a[i]-START)%mod);
-	v.push_back(0),reverse(v.begin(),v.end()),v.push_back(0),v.pop_back();
-	return v;
-}
+#define ll long long
 
-vector <long long> calculate_power(int n,long long mod)
-{
-	vector <long long> p;
-	p.push_back(1);
-	for(int i=0;i<n;i++) p.push_back((p.back()*CNT)%mod);
-	return p;
-}
+class Hashing {
 
-long long sub_left_hash(vector <long long> &v,vector <long long> &p,int l,int r,int mod)
-{
-	return (v[r]-(v[l-1]*p[r-l+1])%mod+mod)%mod;
-}
+	const int BASE = 256;
+	const vector <int> MODS = {1000000007, 573161801};
 
-long long sub_right_hash(vector <long long> &v,vector <long long> &p,int l,int r,int mod)
-{
-	swap(l,r);
-	return (v[r]-(v[l+1]*p[l-r+1])%mod+mod)%mod;
-}
-
-vector <int> longest_palin_odd(string a,vector <vector <long long>> lh,vector <vector <long long>> rh,vector <int> mod,vector <vector <long long>> p)
-{
-	a="$"+a;
-	int n=a.size()-1;
-	vector <int> v;
-	for(int i=1;i<=n;i++)
-	{
-		int lo=1,hi=min(i,n-i+1),mid,ans=-1;
-		while(lo<=hi)
-		{
-			mid=(lo+hi)/2;
-
-			bool okay=true;
-			for(int j=0;j<lh.size() and okay;j++)
-				if(sub_left_hash(lh[j],p[j],i-mid+1,i+mid-1,mod[j])!=sub_right_hash(rh[j],p[j],i-mid+1,i+mid-1,mod[j]))
-					okay=false;
-			if(okay) lo=mid+1,ans=2*mid-1;
-			else hi=mid-1;
-		}
-		v.push_back(ans);
-	}
-	return v;
-}
-
-vector <int> longest_palin_even(string a,vector <vector <long long>> lh,vector <vector <long long>> rh,vector <int> mod,vector <vector <long long>> p)
-{
-	a="$"+a;
-	int n=a.size()-1;
-	vector <int> v;
-	for(int i=1;i<=n;i++)
-	{
-		int lo=1,hi=min(i,n-i),mid,ans=-1;
-		while(lo<=hi)
-		{
-			mid=(lo+hi)/2;
-
-			bool okay=true;
-			for(int j=0;j<lh.size() and okay;j++)
-				if(sub_left_hash(lh[j],p[j],i-mid+1,i+mid,mod[j])!=sub_right_hash(rh[j],p[j],i-mid+1,i+mid,mod[j]))
-					okay=false;
-			if(okay) lo=mid+1,ans=2*mid;
-			else hi=mid-1;
-		}
-		v.push_back(ans);
-	}
-	return v;
-}
-
-int main()
-{
-	//https://www.interviewbit.com/problems/longest-palindromic-substring/
+	int n;
 	string a;
-	cin>>a;
+	vector <vector <ll>> lh, rh, p;
 
-	vector <vector <long long>> left_hash,right_hash,power;
-	for(auto mod:MOD)
-	{
-		left_hash.push_back(calculate_left_hash(a,mod));
-		right_hash.push_back(calculate_right_hash(a,mod));
-		power.push_back(calculate_power(a.size(),mod));
+public:
+
+	void calculate_hashes() {
+		lh.clear();
+		rh.clear();
+		p.clear();
+
+		for(int mod: MODS) {
+			lh.push_back({0});
+			p.push_back({1});
+			for(int x: a) {
+				ll v = lh.back().back() * BASE + x;
+				lh.back().push_back(v % mod);
+				p.back().push_back(p.back().back() * BASE % mod);
+			}
+
+			reverse(a.begin(), a.end());
+			rh.push_back({0});
+			for(int x: a) {
+				ll v = rh.back().back() * BASE + x;
+				rh.back().push_back(v % mod);
+			}
+			reverse(a.begin(), a.end());
+		}
 	}
 
-	vector <int> odd=longest_palin_odd(a,left_hash,right_hash,MOD,power);
-	vector <int> even=longest_palin_even(a,left_hash,right_hash,MOD,power);
+	// a[l, r] is palindrome(1 based indexing)
+	bool pal(int l, int r) {
+		for(int i = 0; i < MODS.size(); i++) {
+			ll lhash = lh[i][r] - lh[i][l - 1] * p[i][r - l + 1];
+			lhash %= MODS[i];
+			if(lhash < 0) lhash += MODS[i];
 
-	for(auto i:odd) cout<<i<<" ";cout<<endl;
-	for(auto i:even) cout<<i<<" ";cout<<endl;
-}
+			ll rhash = rh[i][n - l + 1] - rh[i][n - r] * p[i][r - l + 1];
+			rhash %= MODS[i];
+			if(rhash < 0) rhash += MODS[i];
+
+			if(lhash != rhash) return false;
+		}
+		return true;
+	}
+
+	string longest_pal() {
+		int ans = 0, idx = -1;
+		for(int i = 1; i + 1 < n; i++) {
+			int lo = 1, hi = min(i + 1, n - i), len = -1;
+			while(lo <= hi) {
+				int mid = (lo + hi) >> 1;
+				if(pal(i - mid + 1, i + mid - 1)) lo = mid + 1, len = mid;
+				else hi = mid - 1;
+			}
+
+			int start = i - len + 1;
+			if(a[i] == '#') len = (len + 1) / 2;
+			else len = (len - 2) / 2;
+			if(ans < len) {
+				ans = len;
+				idx = start;
+			}
+		}
+
+		string s;
+		while(s.size() < ans) {
+			if(a[idx] != '#') {
+				s += a[idx];
+				idx++;
+			}
+		}
+		return s;
+	}
+
+
+	void init(string s) {
+		a = "";
+		for(char c: s) {
+			a += c;
+			a += "#";
+		}
+
+		n = a.size();
+		calculate_hashes();
+	}
+};
