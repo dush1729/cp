b87df51f1b3f64e2e16bcad2c4b490a246b4e67f
dush1729 <dush1729@gmail.com>
2020-12-25 11:56:03 +0530

    fix overflow for random

--- library/misc/rand.cpp
+++ library/misc/rand.cpp
@@ -1,12 +1,26 @@
-#include "bits/stdc++.h"
-using namespace std;
+/*
+Description
+return random number from range l to r
 
+Input
+ints l and r are optional
+default values of l and r from 0 to datatype maximum value
 
+Return
+random number from range l to r
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+Problems
+https://codeforces.com/contest/1200/problem/E
+*/
 
-	mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-	cout << rng() << endl; // to generate random number
+int get_rand(int l = 0, int r = INT_MAX) {
+	static mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+	uniform_int_distribution <int> distribution(l, r);
+	return distribution(rng);
+}
+
+long long get_randll(long long l = 0, long long r = LLONG_MAX) {
+	static mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+	uniform_int_distribution <long long> distribution(l, r);
+	return distribution(rng);
 }
