e562fdad64f97f9545a88addbfb65de407b4f6f6
dush1729 <dush1729@gmail.com>
2021-01-01 19:31:39 +0530

    refactor fenwick tree

--- library/query/binary_indexed_tree.cpp
+++ library/query/binary_indexed_tree.cpp
@@ -1,30 +1,20 @@
-#include<bits/stdc++.h>
-using namespace std;
+/*
+Problems
+https://codeforces.com/contest/1354/problem/D
+*/
 
-const int N=1e7+20;
+const int N = 1e7 + 20;
 
-int n,tree[N],a[N];
+int tree[N];
 
-void update(int x,int delta)
-{
-	for(;x<=n;x+=x&-x) tree[x]+=delta;
+// add delta to x position
+void update(int x, int delta) {
+	for(; x <= n; x += x & -x) tree[x] += delta;
 }
 
-int query(int x)
-{
-	int sum=0;
-	for(;x>0;x-=x&-x) sum+=tree[x];
+// returns sum[1 .. x]
+int query(int x) {
+	int sum = 0;
+	for(; x > 0; x -= x & -x) sum += tree[x];
 	return sum;
-}
-
-int main()
-{
-	scanf("%d",&n);
-	for(int i=1;i<=n;i++)
-	{
-		scanf("%d",&a[i]);
-		update(i,a[i]);
-	}
-	printf("sum of first 3 elements is %d\n",query(3));
-	printf("sum of all elements in range [2, 3] is %d\n",query(3)-query(2-1));
 }
