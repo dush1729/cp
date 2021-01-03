d316d4db9a275d3149d740be64d3ef338512138a
dush1729 <dush1729@gmail.com>
2021-01-03 06:35:23 +0530

    add monoqueue

---
+++ library/ds/monoqueue.cpp
@@ -0,0 +1,16 @@
+/*
+Problems
+https://binarysearch.com/problems/Minimal-Submatrices
+*/
+
+vector <int> f(vector <int> &a, int k) {
+    deque <int> dq;
+    vector <int> ans;
+    for(int i = 0; i < a.size(); i++) {
+        while(!dq.empty() and i - dq.front() >= k) dq.pop_front();
+        while(!dq.empty() and a[i] <= a[dq.back()]) dq.pop_back();
+        dq.push_back(i);
+        if(i >= k - 1) ans.push_back(a[dq.front()]);
+    }
+    return ans;
+}
