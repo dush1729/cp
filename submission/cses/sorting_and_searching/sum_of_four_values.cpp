112835290d82ea212d067cd2db1a90d32cb5d888
dush1729 <dush1729@gmail.com>
2021-01-06 02:58:32 +0530

    remove redundant pairs

--- submission/cses/sorting_and_searching/sum_of_four_values.cpp
+++ submission/cses/sorting_and_searching/sum_of_four_values.cpp
@@ -3,30 +3,28 @@ using namespace std;
  
 const int N = 5e3 + 20;
  
-int n, s;
-pair <int, int> p[N];
+int n, a[N], s;
 map <int, pair <int, int>> m;
 
 int main() {
 	scanf("%d%d", &n, &s);
 	for(int i = 0; i < n; i++) {
-		scanf("%d", &p[i].first);
-		p[i].second = i + 1;
+		scanf("%d", &a[i]);
 	}
 
 	for(int i = 0; i < n; i++) {
 		for(int j = i + 1; j < n; j++) {
-			int target = s - (p[i].first + p[j].first);
+			int target = s - (a[i] + a[j]);
 			
 			auto it = m.find(target);
 			if(it != m.end()) {
-				printf("%d %d %d %d", it->second.first, it->second.second, p[i].second, p[j].second);
+				printf("%d %d %d %d", it->second.first, it->second.second, i + 1, j + 1);
 				return 0;
 			}
 		}
 
 		for(int j = 0; j < i; j++) {
-			m[p[i].first + p[j].first] = {p[i].second, p[j].second};
+			m[a[i] + a[j]] = {i + 1, j + 1};
 		}
 	}
 
