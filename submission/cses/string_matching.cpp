810700a2523c85c17db07f5138bceb99f64a44c7
dush1729 <dush1729@gmail.com>
2021-01-22 19:10:48 +0530

    CSES: String Matching

---
+++ submission/cses/string_matching.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+const int BASE = 26, START = 'a', MOD = 1e9 + 7;
+
+string a, b;
+int n, m, ha, hb, p = 1;
+
+int add(int x, int y) {
+	return (0ll + x + y) % MOD;
+}
+
+int mul(int x, int y) {
+	return (1ll * x * y) % MOD;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> a >> b;
+	n = a.size(), m = b.size();
+
+	if(n < m) {
+		cout << 0;
+		return 0;
+	}
+
+	for(int i = 0; i < m; i++) {
+		hb = add(mul(hb, BASE), b[i] - START);
+		if(i + 1 != b.size()) p = mul(p, BASE);
+	}
+
+	for(int i = 0; i < m; i++) {
+		ha = add(mul(ha, BASE), a[i] - START);
+	}
+
+	int ans = (ha == hb);
+	for(int i = m; i < n; i++) {
+		ha = (ha - mul(a[i - m] - START, p) + MOD) % MOD;
+		ha = add(mul(ha, BASE), a[i] - START);
+		ans += (ha == hb);
+	}
+
+	cout << ans;
+}
