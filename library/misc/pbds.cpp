d951af49de4965890ae085ab49cd6ddd40546f14
dush1729 <dush1729@gmail.com>
2021-02-02 14:42:17 +0530

    refactor pbds

--- library/misc/pbds.cpp
+++ library/misc/pbds.cpp
@@ -1,17 +1,35 @@
+/*
+Blog
+https://codeforces.com/blog/entry/11080
+
+Problems
+https://codeforces.com/problemset/problem/61/E
+https://cses.fi/problemset/task/1076
+
+Description
+pbds is basically set on steroids!
+It can perform all operations as efficiently as set.
+Plus it can find kth smallest value & rank in logn time for which set takes linear time.
+
+find kth smallest value(0 based indexing)
+*s.find_by_order(k)
+
+rank of number i.e. number of items < k
+s.order_of_key(k)
+
+BEWARE in case of duplicates, use pair <int, int>
+*/
+
 #include "bits/stdc++.h"
+using namespace std;
+
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
-using namespace std;
 
 typedef tree<
 int, null_type,
 less<int>, rb_tree_tag, tree_order_statistics_node_update>
 ordered_set;
-ordered_set s;
-//*s.find_by_order(0) to find 0th element in sorted list
-//s.order_of_key(k) to find number of items < k
 
-int main()
-{
-	//https://codeforces.com/blog/entry/11080
+int main() {
 }
