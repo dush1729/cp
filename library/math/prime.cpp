cdc809979ece1551c0e9563f9caabcab3d46ffed
dush1729 <dush1729@gmail.com>
2021-02-04 23:50:29 +0530

    add list of primes

--- library/math/prime.cpp
+++ library/math/prime.cpp
@@ -4,16 +4,22 @@ using namespace std;
 const int MAX = 1e6 + 20;
 
 int lp[MAX];
+vector <int> prime;
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
+void preprocess() {
 	// find least prime divisor for all from [1, MAX - 1]
 	for(int i = 2; i * i < MAX; i++)
 		if(!lp[i])
 			for(int j = i * i; j < MAX; j += i)
 				if(!lp[j]) lp[j] = i;
-	for(int i = 1; i < MAX; i++)
+	for(int i = 2; i < MAX; i++) {
 		if(!lp[i]) lp[i] = i;
+		if(lp[i] == i) prime.push_back(i);
+	}
+	lp[1] = 1;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 }
