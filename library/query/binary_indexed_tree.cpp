7ae2661709b7acc2e5888f33143f4904c3e96b46
dush1729 <dush1729@gmail.com>
2021-04-01 11:40:25 +0530

    refactor fenwick tree

--- library/query/binary_indexed_tree.cpp
+++ library/query/binary_indexed_tree.cpp
@@ -1,20 +1,25 @@
 /*
 Problems
 https://codeforces.com/contest/1354/problem/D
+https://binarysearch.com/problems/Bubble-Swap
 */
 
-const int N = 1e7 + 20;
+struct BIT {
+	int n;
+	vector <int> tree;
+	BIT(int SZ): tree(SZ + 1) {
+		n = SZ;
+	}
 
-int tree[N];
+	// a[x] += delta
+	void update(int x, int delta) {
+		for(; x <= n; x += x & -x) tree[x] += delta;
+	}
 
-// add delta to x position
-void update(int x, int delta) {
-	for(; x <= n; x += x & -x) tree[x] += delta;
-}
-
-// returns sum[1 .. x]
-int query(int x) {
-	int sum = 0;
-	for(; x > 0; x -= x & -x) sum += tree[x];
-	return sum;
-}
+	// sum from a[1] to a[x]
+	int query(int x) {
+		int sum = 0;
+		for(; x > 0; x -= x & -x) sum += tree[x];
+		return sum;
+	}
+};
