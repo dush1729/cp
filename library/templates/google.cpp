66059e072961097888cdf24213139866f952817f
dush1729 <dush1729@gmail.com>
2021-04-11 14:34:49 +0530

    update google template

--- library/templates/google.cpp
+++ library/templates/google.cpp
@@ -1,18 +1,24 @@
 #include "bits/stdc++.h"
 using namespace std;
+#define ll long long
+const int areTests = 1;
 
 
 
-int t;
+void run_test(int testcase) {
+	cout << "Case #" << testcase << ": ";
+}
 
 int main() {
 	ios::sync_with_stdio(0);
+	#ifndef LOCAL
 	cin.tie(0);
+	#endif
+	cout << fixed << setprecision(10);
 
-	cin >> t;
-	for(int tc = 1; tc <= t; tc++) {
-
-
-		cout << "Case #" << tc << ": ";
+	int t = 1;
+	if(areTests) cin >> t;
+	for(int i = 1; i <= t; i++) {
+		run_test(i);
 	}
 }
