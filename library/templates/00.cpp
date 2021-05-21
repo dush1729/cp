eb2e7f01387a82a59c59325885484dfac2d0a79b
dush1729 <dush1729@gmail.com>
2021-05-21 10:39:35 +0530

    update skeleton template

--- library/templates/00.cpp
+++ library/templates/00.cpp
@@ -1,11 +1,23 @@
 #include "bits/stdc++.h"
 using namespace std;
+#define ll long long
+const int areTests = 0;
 
 
+
+void run_test(int testcase) {
+}
 
 int main() {
 	ios::sync_with_stdio(0);
+	#ifndef DUSH1729
 	cin.tie(0);
+	#endif
+	cout << fixed << setprecision(10);
 
-	
+	int t = 1;
+	if(areTests) cin >> t;
+	for(int i = 1; i <= t; i++) {
+		run_test(i);
+	}
 }
