a78f9375c1766a5213e672389e9fce70c685f3c0
dush1729 <dush1729@gmail.com>
2021-05-25 16:32:00 +0530

    refactor leetcode template

--- library/templates/leetcode.cpp
+++ library/templates/leetcode.cpp
@@ -3,14 +3,33 @@ using namespace std;
 #define ll long long
 
 
+
 class Solution {
 public:
+
 	int maxSumDivThree() {return 0;
+	}
+
+
+	int testcase = 0;
+	void test(int expected) {
+		auto output = maxSumDivThree();
+		if(expected != output) {
+			cout << "Failed at test " << ++testcase << endl;
+			cout << "expected : " << expected << endl;
+			cout << "output   : " << output << endl;
+			exit(0);
+		} else {
+			cout << "Test " << ++testcase << " passed." << endl;
+		}
+	}
+
+	Solution() {
+		test(0);
+		cout << "All tests passed successfully!" << endl;
 	}
 };
 
 #ifdef DUSH1729
-int main() {
-	auto ans = Solution().maxSumDivThree();
-}
+int main(){Solution();}
 #endif
