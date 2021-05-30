15846fc1a4f08512debe419a3e1ac7866699de58
dush1729 <dush1729@gmail.com>
2021-05-30 09:59:20 +0530

    update leetcode template

--- library/templates/leetcode.cpp
+++ library/templates/leetcode.cpp
@@ -10,21 +10,21 @@ public:
 	}
 };
 
-int testcase = 0;
-void test(int expected) {
-	auto output = Solution().maxSumDivThree();
-	if(expected != output) {
-		cout << "Failed at test " << ++testcase << endl;
-		cout << "expected : " << expected << endl;
-		cout << "output   : " << output << endl;
-		exit(0);
-	} else {
-		cout << "Test " << ++testcase << " passed." << endl;
-	}
-}
-
 #ifdef DUSH1729
 int main(){
+	int testcase = 0;
+	auto test = [&](int expected) {
+		auto output = Solution().maxSumDivThree();
+		if(expected != output) {
+			cout << "Failed at test " << ++testcase << endl;
+			cout << "expected : " << expected << endl;
+			cout << "output   : " << output << endl;
+			exit(0);
+		} else {
+			cout << "Test " << ++testcase << " passed." << endl;
+		}
+	};
+
 	test(0);
 	cout << "All tests passed successfully!" << endl;
 }
