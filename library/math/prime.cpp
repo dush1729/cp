3e9d6b7fad71b7ae0d8456391132e7ea3f381553
dush1729 <dush1729@gmail.com>
2021-08-22 17:11:25 +0530

    linear sieve, closes #5

--- library/math/prime.cpp
+++ library/math/prime.cpp
@@ -1,25 +1,30 @@
-#include "bits/stdc++.h"
-using namespace std;
+// find least prime divisor for all values in [1, MAX - 1]
+// size of prime can be reduced to primepi(MAX)
 
-const int MAX = 1e6 + 20;
+const int MAX = 1e6 + 2;
+int lp[MAX], prime[MAX], prime_cnt; 
+void sieve_linear() {
+	for(int i = 2; i < MAX; i++) {
+		if(lp[i] == 0) {
+			lp[i] = i;
+			prime[prime_cnt++] = i;
+		}
+		for(int j = 0; j < prime_cnt; j++) {
+			int x = prime[j], m = i * x;
+			if(x <= lp[i] && m < MAX) lp[m] = x;
+			else break;
+		}
+	}
+}
 
-int lp[MAX];
-vector <int> prime;
-
-void preprocess() {
-	// find least prime divisor for all from [1, MAX - 1]
+// if you only require lp, this reduces memory by half since prime list not required
+void sieve_nloglogn() {
 	for(int i = 2; i * i < MAX; i++)
 		if(!lp[i])
 			for(int j = i * i; j < MAX; j += i)
 				if(!lp[j]) lp[j] = i;
 	for(int i = 2; i < MAX; i++) {
 		if(!lp[i]) lp[i] = i;
-		if(lp[i] == i) prime.push_back(i);
+		if(lp[i] == i) prime[prime_cnt++] = i;
 	}
-	lp[1] = 1;
-}
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
 }
