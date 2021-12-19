0ae7568f6d1e8e6f599ec09beb8cd7415569a716
dush1729 <dush1729@gmail.com>
2021-12-19 19:47:53 +0530

    update lis template

--- library/binary_search/longest_incresing_subsequence.cpp
+++ library/binary_search/longest_incresing_subsequence.cpp
@@ -1,23 +1,15 @@
-#include "bits/stdc++.h"
-using namespace std;
-
-const int N=2e5+20;
-
-vector <int> v;
-int n,a[N];
-
-int main()
-{
-	//https://www.interviewbit.com/problems/longest-increasing-subsequence/
-	cin>>n;
-	for(int i=1;i<=n;i++) cin>>a[i];
-
-	for(int i=1;i<=n;i++)
-	{
-		int j=lower_bound(v.begin(),v.end(),a[i])-v.begin();
-		if(j==v.size()) v.push_back(a[i]);
-		else v[j]=a[i];
+int lis(vector <int> a) {
+	vector <int> v;
+	for(const int &x: a) {
+		//lower_bound for strictly
+		int i = upper_bound(v.begin(), v.end(), x) - v.begin();
+		if(i == v.size()) v.push_back(x);
+		else v[i] = x;
 	}
+	return v.size();
+}
 
-	cout<<v.size()<<endl;
+int lds(vector <int> &a) {
+	reverse(a.begin(), a.end());
+	return lis(a);
 }
