5677f8a90fb90236b6621108a3586cf399328a7f
dush1729 <dush1729@gmail.com>
2021-12-29 14:43:26 +0530

    update leetcode template

--- library/templates/leetcode.cpp
+++ library/templates/leetcode.cpp
@@ -6,26 +6,10 @@ using namespace std;
 
 class Solution {
 public:
-	int maxSumDivThree() {return 0;
+	void maxPoints(vector<vector<int>>& a) {
 	}
 };
 
 #ifdef DUSH1729
-int main(){
-	int testcase = 0;
-	auto test = [&](int expected) {
-		auto output = Solution().maxSumDivThree();
-		if(expected != output) {
-			cout << "Failed at test " << ++testcase << endl;
-			cout << "expected : " << expected << endl;
-			cout << "output   : " << output << endl;
-			exit(0);
-		} else {
-			cout << "Test " << ++testcase << " passed." << endl;
-		}
-	};
-
-	test(0);
-	cout << "All tests passed successfully!" << endl;
-}
+int main(){cout << "Compiled!";}
 #endif
