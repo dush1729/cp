08710730bd07dc664504de013ed4ea0d3eb0f936
dush1729 <dush1729@gmail.com>
2022-01-10 17:26:11 +0530

    convert spaces to tabs

--- library/ds/monoqueue.cpp
+++ library/ds/monoqueue.cpp
@@ -4,13 +4,13 @@ https://binarysearch.com/problems/Minimal-Submatrices
 */
 
 vector <int> f(vector <int> &a, int k) {
-    deque <int> dq;
-    vector <int> ans;
-    for(int i = 0; i < a.size(); i++) {
-        while(!dq.empty() and i - dq.front() >= k) dq.pop_front();
-        while(!dq.empty() and a[i] <= a[dq.back()]) dq.pop_back();
-        dq.push_back(i);
-        if(i >= k - 1) ans.push_back(a[dq.front()]);
-    }
-    return ans;
+	deque <int> dq;
+	vector <int> ans;
+	for(int i = 0; i < a.size(); i++) {
+		while(!dq.empty() and i - dq.front() >= k) dq.pop_front();
+		while(!dq.empty() and a[i] <= a[dq.back()]) dq.pop_back();
+		dq.push_back(i);
+		if(i >= k - 1) ans.push_back(a[dq.front()]);
+	}
+	return ans;
 }
