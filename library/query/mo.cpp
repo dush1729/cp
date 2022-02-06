d0d177c920fb10fa337760d0a9796b430cb55eb7
dush1729 <dush1729@gmail.com>
2022-02-06 17:59:10 +0530

    Library: refactor mo's algo

--- library/query/mo.cpp
+++ library/query/mo.cpp
@@ -1,54 +1,43 @@
-#include<bits/stdc++.h>
-using namespace std;
+/*
+Problems
+https://atcoder.jp/contests/abc238/tasks/abc238_g
+https://codeforces.com/problemset/problem/86/D
+*/
 
-const int N=3e4+20,Q=2e5+20,MAX=1e6+20,SIZE=sqrt(N)+1;
+int sz;
 
-int n,q,a[N],l,r,cl=1,cr=1,f[MAX],ans[Q],cnt;
-vector <pair<pair<int,int>,int>> p;
+struct query {
+	int l, r, i;
+	bool operator<(const query &T) const {
+		if (l / sz != T.l / sz) return l < T.l;
+		return (l / sz) & 1 ? r < T.r : r > T.r;
+	}
+};
 
-bool cmp(pair<pair<int,int>,int> &a, pair<pair<int,int>,int> &b)
-{
-    if(a.first.first/SIZE!=b.first.first/SIZE) return a.first.first<b.first.first;
-    return a.first.second<b.first.second;
-}
+int main() {
+	//read n, prefer local over global variables(see abc238g submissions)
+	sz = sqrt(n);
 
-void add(int idx)
-{
-    if(!f[a[idx]]) cnt++;
-    f[a[idx]]++;
-}
+	vector <query> p(q);
+	for(int i = 0; i < q; i++) {
+		cin >> p[i].l >> p[i].r;
+		p[i].l--, p[i].r--;
+		p[i].i = i;
+	}
 
-void remove(int idx)
-{
-    if(f[a[idx]]==1) cnt--;
-    f[a[idx]]--;
-}
+	int cl = 0, cr = -1, cnt = 0;
+	auto add = [&](int i) {
+	};
+	auto remove = [&](int i) {
+	};
 
-int main()
-{
-    //https://codeforces.com/contest/86/problem/D
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++) scanf("%d",&a[i]);
-    scanf("%d",&q);
-    for(int i=0;i<q;i++)
-    {
-        scanf("%d%d",&l,&r);
-        p.push_back({{l,r},i});
-    }
-
-    sort(p.begin(),p.end(),cmp);
-
-    for(int i=0;i<q;i++)
-    {
-        l=p[i].first.first,r=p[i].first.second;
-
-        while(cl<l) remove(cl++);
-        while(cl>l) add(--cl);
-        while(cr>r+1) remove(--cr);
-        while(cr<r+1) add(cr++);
-
-        ans[p[i].second]=cnt;
-    }
-
-    for(int i=0;i<q;i++) printf("%d\n",ans[i]);
+	sort(p.begin(), p.end());
+	vector <int> ans(q);
+	for(auto &[l, r, idx]: p) {
+		while(cl > l) add(--cl);
+		while(cr < r) add(++cr);
+		while(cl < l) remove(cl++);
+		while(cr > r) remove(cr--);
+		ans[idx] = cnt;
+	}
 }
