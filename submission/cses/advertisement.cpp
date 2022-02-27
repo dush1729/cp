b529c857d28770f5665d9b73aa4194747867575c
dush1729 <dush1729@gmail.com>
2022-02-27 11:51:55 +0530

    CSES: Advertisement

---
+++ submission/cses/advertisement.cpp
@@ -0,0 +1,15 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+
+
+
+class Solution {
+public:
+	int minimumFinishTime(vector<vector<int>>& tires, int changeTime, int numLaps) {
+	}
+};
+
+#ifdef DUSH1729
+int main(){cout << "Compiled!";}
+#endif
