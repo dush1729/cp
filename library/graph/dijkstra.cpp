46b8f525f0869e6e3398d7552d358bce7c3699a5
dush1729 <dush1729@gmail.com>
2022-03-13 11:30:22 +0530

    refactor dijkstra

--- library/graph/dijkstra.cpp
+++ library/graph/dijkstra.cpp
@@ -2,58 +2,51 @@
 Problems
 https://codeforces.com/contest/20/problem/C
 https://codeforces.com/contest/1076/problem/D
+https://leetcode.com/problems/minimum-weighted-subgraph-with-the-required-paths
 */
 
-#include "bits/stdc++.h"
-using namespace std;
-#define ll long long
+struct DIJKSTRA {
+	struct node {
+		int city;
+		ll cost;
 
-const int N = 1e5 + 20;
-const ll MAX = 1e18;
+		bool operator<(const node &rhs) const {
+			return cost > rhs.cost;
+		}
+	};
 
-int n, m, u, v, c, p[N];
-long long dp[N];
-vector <pair <int, int>> adj[N];
-priority_queue <pair <ll, int>> pq;
-vector <int> ans;
-bool processed[N];
+	vector <ll> d;
+	vector <int> vis, p;
+	priority_queue <node> pq;
 
-void add(int node, int parent, ll dist) {
-	dp[node] = dist;
-	pq.push({-dist, node});
-	p[node] = parent;
-}
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	cin >> n >> m;
-	while(m--) {
-		cin >> u >> v >> c;
-		adj[u].push_back({c, v});
-		adj[v].push_back({c, u});
+	void add(int node, int parent, ll dist) {
+		pq.push({node, dist});
+		d[node] = dist;
+		p[node] = parent;
 	}
 
-	for(int i = 1; i <= n; i++) dp[i] = MAX;
+	DIJKSTRA(const vector <vector <pair <int, int>>> &g, int source) {
+		int n = g.size();
+		d.resize(n, 1e18), vis.resize(n, 0), p.resize(n, -1);
 
-	add(1, 0, 0);
-	while(!pq.empty()) {
-		int node=pq.top().second;
-		pq.pop();
-		if(processed[node]) continue;
+		add(source, -1, 0);
+		while(!pq.empty()) {
+			auto [node, dist] = pq.top();
+			pq.pop();
+			if(vis[node]) continue;
+			vis[node] = 1;
 
-		processed[node] = true;
-		for(auto &[cost, child]: adj[node]) {
-			if(dp[child] <= dp[node] + cost) continue;
-			add(child, node, dp[node] + cost);
+			for(const auto &[child, c]: g[node])
+				if(d[child] > dist + c) add(child, node, dist + c);
 		}
 	}
 
-	if(p[n] == 0) cout << -1;
-	else {
-		for(int i = n; i != 0; i = p[i]) ans.push_back(i);
-		reverse(ans.begin(), ans.end());
-		for(auto i: ans) cout << i << " ";
+	vector <int> get_path(int i) {
+		if(!vis[i]) return {};
+
+		vector <int> path;
+		for( ; i != -1; i = p[i]) path.push_back(i);
+		reverse(path.begin(), path.end());
+		return path;
 	}
-}
+};
