95f0d4e7b300de9f5650675ce3575d94a9fbb100
dush1729 <dush1729@gmail.com>
2022-11-14 02:32:44 +0530

    dsu minor refactor

--- library/ds/dsu.cpp
+++ library/ds/dsu.cpp
@@ -10,28 +10,13 @@ struct DSU {
 		int parent, size;
 		node(int id = 0): parent(id), size(1) {}
 	};
-
 	vector <node> data;
 
-	DSU(int SZ = 0): count(SZ), maxsize(1), data(SZ + 1) {
-		iota(data.begin(), data.end(), 0);
-	}
-
-	int root(int i) {
-		return i == data[i].parent ? data[i].parent : root(data[i].parent);
-	}
-
-	bool is_root(int i) {
-		return i == root(i);
-	}
-
-	bool cycle(int a, int b) {
-		return root(a) == root(b);
-	}
-
-	int set_size(int a) {
-		return data[root(a)].size;
-	}
+	DSU(int SZ = 0): count(SZ), maxsize(1), data(SZ + 1) {iota(data.begin(), data.end(), 0);}
+	int root(int i) {return i == data[i].parent ? i : root(data[i].parent);}
+	bool is_root(int i) {return i == root(i);}
+	bool cycle(int a, int b) {return root(a) == root(b);}
+	int set_size(int a) {return data[root(a)].size;}
 
 	bool join(int a, int b) {
 		a = root(a), b = root(b);
@@ -39,9 +24,7 @@ struct DSU {
 
 		count--;
 		if(data[a].size < data[b].size) swap(a, b);
-		data[b].parent = a;
-		data[a].size += data[b].size;
-		maxsize = max(maxsize, data[a].size);
+		data[b].parent = a, data[a].size += data[b].size, maxsize = max(maxsize, data[a].size);
 
 		return true;
 	}
