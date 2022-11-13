9462e55941c8f1a31b3a6a91d4eff9bbe526c370
dush1729 <dush1729@gmail.com>
2022-11-13 13:00:06 +0530

    minor refactor binary indexed tree

--- library/query/binary_indexed_tree.cpp
+++ library/query/binary_indexed_tree.cpp
@@ -6,17 +6,18 @@ https://binarysearch.com/problems/Bubble-Swap
 
 struct BIT {
 	int n;
-	vector <int> tree;
+	vector <ll> tree;
 	BIT(int SZ): tree(SZ + 1), n(SZ) {}
 
 	// a[x] += delta
-	void update(int x, int delta) {
+	void update(int x, ll delta) {
+		assert(x > 0);
 		for(; x <= n; x += x & -x) tree[x] += delta;
 	}
 
 	// sum from a[1] to a[x]
-	int query(int x) {
-		int sum = 0;
+	ll query(ll x) {
+		ll sum = 0;
 		for(; x > 0; x -= x & -x) sum += tree[x];
 		return sum;
 	}
