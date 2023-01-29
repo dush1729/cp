3f90b12802d2896aa04035611141f59c77189a35
Dushyant Singh <dush1729@gmail.com>
2023-01-30 03:51:20 +0800

    add kahn to topological sort

--- library/graph/topological_sort.cpp
+++ library/graph/topological_sort.cpp
@@ -2,6 +2,8 @@
 Problems
 https://cses.fi/problemset/task/1681
 https://codeforces.com/contest/919/problem/D
+https://leetcode.com/problems/course-schedule-ii/description/
+https://codeforces.com/contest/1790/problem/C
 */
 
 struct TOPOLOGICAL_SORT {
@@ -27,6 +29,35 @@ struct TOPOLOGICAL_SORT {
 			if(!vis[i]) dfs(i);
 
 		// uncomment for root -> leaf
+		//reverse(order.begin(), order.end());
+	}
+};
+
+struct KAHN {
+	// root -> leaf
+	vector <int> order;
+	int cycle = 0;
+
+	KAHN(const vector <vector <int>> &adj) {
+		int n = adj.size();
+		vector indeg(n, 0);
+		for(int node = 0; node < n; node++)
+			for(const auto &child: adj[node]) indeg[child]++;
+
+		priority_queue<int, vector<int>, greater<int>> pq; // lexicographical min
+		for(int i = 0; i < n; i++)
+			if(indeg[i] == 0) pq.push(i);
+
+		while(!pq.empty()) {
+			int node = pq.top();
+			pq.pop();
+			order.push_back(node);
+			for(const auto &child: adj[node])
+				if(--indeg[child] == 0) pq.push(child);
+		}
+		if(order.size() != n) cycle = 1, order.clear();
+
+		// uncomment for leaf -> root
 		//reverse(order.begin(), order.end());
 	}
 };
