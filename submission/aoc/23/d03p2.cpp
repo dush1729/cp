f8a1cee7f3603f38cad82d315a70a4fad21c12cb
dush1729 <dush1729@gmail.com>
2023-12-03 10:57:16 +0530

    aoc day 3

---
+++ submission/aoc/23/d03p2.cpp
@@ -0,0 +1,72 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+const int areTests = 0, N = 140;
+
+
+
+void run_test(int testcase) {
+	vector <string> a(N);
+	for(int i = 0; i < N; i++) cin >> a[i];
+
+	auto in = [&](int i, int j) {
+		return i >= 0 && j >= 0 && i < N && j < a[i].size();
+	};
+
+	vector <ll> gear[N][N];
+	auto gear_insert = [&](const vector <pair <int, int>> &points, ll val) {
+		set <pair <int, int>> s;
+		for(auto &[x, y]: points) {
+			for(int dx = -1; dx <= 1; dx++) {
+				for(int dy = -1; dy <= 1; dy++) {
+					int nx = x + dx, ny = y + dy;
+					if(in(nx, ny) && a[nx][ny] == '*') s.insert({nx, ny});
+				}
+			}
+		}
+		for(auto &[x, y]: s) {
+			gear[x][y].push_back(val);
+		}
+	};
+
+	ll ans = 0;
+	for(int i = 0; i < N; i++) {
+		int m = a[i].size();
+		for(int j = 0; j < m; ) {
+			for( ; j < m && !isdigit(a[i][j]); j++) ;
+			if(j >= m) break;
+
+			vector <pair <int, int>> points;
+			string num;
+			for( ; j < m && isdigit(a[i][j]); j++) {
+				points.push_back({i, j});
+				num += a[i][j];
+			}
+			ll val = stoll(num);
+
+			gear_insert(points, val);
+		}
+	}
+
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < a[i].size(); j++) {
+			if(gear[i][j].size() != 2) continue;
+			ans += gear[i][j][0] * gear[i][j][1];
+		}
+	}
+	cout << ans;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	#ifndef DUSH1729
+	cin.tie(0);
+	#endif
+	cout << fixed << setprecision(10);
+
+	int t = 1;
+	if(areTests) cin >> t;
+	for(int i = 1; i <= t; i++) {
+		run_test(i);
+	}
+}
